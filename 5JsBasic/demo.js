12523331387531a3205bb28e8c7d7612a1a5a84f
Lidai <2509129925@qq.com>
2021-02-09 08:58:29 +0800

    红js红宝书

--- 5JsBasic/demo.js
+++ 5JsBasic/demo.js
@@ -64,3 +64,92 @@ console.log(colors2)
 let newMap = new Map([["key1","value1"]])
 Map["key1"]="value2"
 console.log(newMap.get("key1"))
+let array = [1,2,3,4]
+let iterator =array[Symbol.iterator]();
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+console.log(iterator.next())
+class Couter
+{
+    constructor(limit)
+    {
+        this.limit=limit;
+    }
+    [Symbol.iterator](){
+        let count=1;
+        let limit=this.limit;
+        return{
+            next(){
+                if(count<=limit)
+                {
+                    return{done:false,value:count++}
+                }else{
+                    return{
+                        done:true,value:undefined
+                    }
+                }
+            },
+            return(){
+                    console.log("Exiting");
+                    return{done:true}
+            }
+        }
+    }
+}
+let counter =new Couter(5)
+for(let i of counter)
+{
+    if(i>2)break
+    console.log(i)
+}
+function* f()
+{
+    console.log(1)
+    yield 'foo';
+    yield 'bar';
+    return 'aa'
+}
+let foo = f();
+console.log(foo.next())
+console.log(foo.next())
+let person = { 
+    name_:'',
+    age:18,
+    get name() {
+        return this.name_
+    },
+    set name(name) {
+        this.name_ =name
+    },
+    sayName(){
+        console.log(this.name_)
+    }
+   };
+   person.name = 'Matt'; 
+   person.sayName(); // My name is Matt
+function Person(name){
+    this.name=name;
+}
+let p1 = new Person('lmy')
+p1.__proto__.age=18
+console.log(Person.prototype)
+console.log(Object.values(person))
+console.log(Object.entries(person))
+console.log('-0-------------------')
+class Peopel{
+    constructor(name,age)
+    {
+        this.name=name;
+        this.age=age;
+        this.sex='female';
+    }
+    sex ='male';
+    locate(){
+        console.log(1)
+    }
+}
+let peo1 =new Peopel('lmy',18);
+console.log(peo1.name)
+peo1.locate()
